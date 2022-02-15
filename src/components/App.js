0e185b0e966b1be36bb533885729172408a503f6
joselawis <joselawis@gmail.com>
2022-02-15 13:47:36 +0100

    Ejemplo shouldComponentUpdate

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import Footer from "../shared/components/layout/Footer";
 
 import "./App.css";
 import Timer from "./Pomodoro/Timer";
+import Coins from "./Coins/Coins";
 
 function App() {
   return (
     <div className="App">
       <Header title="Welcome to Lawis React App" />
       <Content>
+        <Coins/>
         <Timer />
         <Todo />
       </Content>
---
+++ src/components/Coins/Coins.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "./Coins.css";
+
+class Coins extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      dollars: 0,
+    };
+  }
+
+  shouldComponentUpdate(props, state) {
+    // El componente se actualiza si cumple la condición
+    return state.dollars % 10 === 0;
+  }
+
+  handleOnChange = (e) => {
+    this.setState({
+      dollars: Number(e.target.value || 0),
+    });
+  };
+
+  render() {
+    return (
+      <div className="Coins">
+        <h1>Buy Crypto Coins!</h1>
+        <div className="question">
+          <p>How much dollars do you have?</p>
+          <p>
+            <input placeholder="0" onChange={this.handleOnChange} type="text" />
+          </p>
+        </div>
+        <div className="answer">
+          <p>Crypto coin price: $10</p>
+          <p>
+            You can buy <strong>{this.state.dollars / 10}</strong> coins.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Coins;
