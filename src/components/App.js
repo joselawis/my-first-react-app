73ee7f20913e5ab3997645033fb51bb7d7d31fda
joselawis <joselawis@gmail.com>
2022-02-16 12:44:40 +0100

    ComponentDidUpdate

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,14 @@ import Todo from "./Todo/Todo";
 import Timer from "./Pomodoro/Timer";
 import Coins from "./Coins/Coins";
 import NotesApp from "./Notes/NotesApp";
+import ChartApp from "./Chart/ChartApp";
 
 function App() {
   return (
     <div className="App">
       <Header title="Welcome to Lawis React App" />
       <Content>
+        <ChartApp />
         <NotesApp />
         <Coins />
         <Timer />
