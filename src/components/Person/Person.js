fa6e63f1554ee54d52d80bee3d72b664287af43b
joselawis <joselawis@gmail.com>
2022-02-19 00:17:47 +0100

    check form data and error handling

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -10,6 +10,10 @@ class Person extends Component {
       lastName: "",
       email: "",
       phone: "",
+      errors: {
+        firstName: false,
+        lastName: false,
+      },
     };
   }
 
@@ -23,35 +27,61 @@ class Person extends Component {
     });
   };
 
+  handleOnSubmit = (e) => {
+    // El método e.preventDefault() cancela el evento si este es cancelable, por lo ue la acción predeterminada que pertenece al evento, no se producirá.
+    e.preventDefault();
+
+    const { firstName, lastName, email, phone } = this.state;
+
+    this.setState({
+      errors: {
+        firstName: firstName === "",
+        lastName: lastName === "",
+      },
+    });
+    const data = {
+      firstName,
+      lastName,
+      email,
+      phone,
+    };
+
+    console.log("Data:", data);
+  };
+
   render() {
     return (
       <div className="Person">
-        <form>
+        <form onSubmit={this.handleOnSubmit}>
           <div>
             <p>
               <strong>Name:</strong>
             </p>
-            <p>
-              <input
-                name="firstName"
-                type="text"
-                value={this.state.firstName}
-                onChange={this.handleOnChange}
-              />
-            </p>
+            <input
+              name="firstName"
+              type="text"
+              value={this.state.firstName}
+              onChange={this.handleOnChange}
+              className={this.state.errors.firstName ? "error" : ""}
+            />
+            {this.state.errors.firstName && (
+              <div className="errorMessage">Campo obligatorio</div>
+            )}
           </div>
           <div>
             <p>
               <strong>Last Name:</strong>
             </p>
-            <p>
-              <input
-                name="lastName"
-                type="text"
-                value={this.state.lastName}
-                onChange={this.handleOnChange}
-              />
-            </p>
+            <input
+              name="lastName"
+              type="text"
+              value={this.state.lastName}
+              onChange={this.handleOnChange}
+              className={this.state.errors.lastName ? "error" : ""}
+            />
+            {this.state.errors.lastName && (
+              <div className="errorMessage">Campo obligatorio</div>
+            )}
           </div>
           <div>
             <p>
